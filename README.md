dd4f4d3a5e8b5d981810421aa7133e620fc8d5bf
George Campbell <campbell.goe@gmail.com>
2024-05-29 17:05:32 +0100

    update python script to use argv ments

--- README.md
+++ README.md
@@ -4,10 +4,4 @@ first edit scrape-jobs.py to include tour search term.
 
 then on the command line run the python script.
 
-`python3 scrape-jobs.py`
-
-# improvements
-
-you could code it so it takes an argument as the search term
-
-# good luck
+`python3 scrape-jobs.py <search_term>`
--- scrape-jobs.py
+++ scrape-jobs.py
@@ -1,17 +1,28 @@
 import csv
 from jobspy import scrape_jobs
 from datetime import date
-search_term = "react.js three.js next.js"
+import sys
+
+# Handle error and exit with a message on empty argument
+if len(sys.argv) < 2:
+    print("Error: No search term provided.")
+    print("Usage: python script_name.py <search_term>")
+    sys.exit(1)
+
+search_term = sys.argv[1]
+
 jobs = scrape_jobs(
     site_name=["indeed", "linkedin", "zip_recruiter", "glassdoor"],
     search_term=search_term,
     location="United Kingdom",
     results_wanted=20,
-    hours_old=72, # (only Linkedin/Indeed is hour specific, others round up to days old)
+    hours_old=72,  # (only Linkedin/Indeed is hour specific, others round up to days old)
     country_indeed='UK'  # only needed for indeed / glassdoor
 )
+
 print(f"Found {len(jobs)} jobs")
 print(jobs.head())
+
 today = date.today()
 thedate = today.strftime("%d-%m-%Y")
-jobs.to_csv(search_term+"_"+thedate+"_jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False) # to_xlsx
+jobs.to_csv(search_term + "_" + thedate + "_jobs.csv", quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False)
